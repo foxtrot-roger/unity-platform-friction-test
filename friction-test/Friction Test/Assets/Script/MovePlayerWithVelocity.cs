5ab28ddcbbd598c423b031b638ab9a12040db252
foxtrot-roger <36508665+foxtrot-roger@users.noreply.github.com>
2021-10-03 21:22:23 +0200

    Added test for player controller

---
+++ friction-test/Friction Test/Assets/Script/MovePlayerWithVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerWithVelocity : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float MoveSpeed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    float horizontal;
+    float vertical;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 || vertical != 0)
+            rb.velocity = new Vector2(horizontal, vertical) * MoveSpeed;
+
+        rb.MoveRotation(Quaternion.identity);
+    }
+}
---
+++ friction-test/Friction Test/Assets/Script/MoveRigidBodyWithVelocity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRigidBodyWithVelocity : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float MoveSpeed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    float horizontal;
+    float vertical;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal, vertical) * MoveSpeed;
+        rb.MoveRotation(Quaternion.identity);
+    }
+}
