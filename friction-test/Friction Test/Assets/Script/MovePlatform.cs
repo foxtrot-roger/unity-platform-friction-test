fb812e2485510a32a6c77ef03ec1d743f29da4b1
foxtrot-roger <36508665+foxtrot-roger@users.noreply.github.com>
2021-10-03 19:54:10 +0200

    Initial commit

---
+++ friction-test/Friction Test/Assets/Script/MovePlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float MoveSpeed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    float horizontal;
+    float vertical;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(horizontal, vertical) * Time.fixedDeltaTime * MoveSpeed);
+        rb.MoveRotation(Quaternion.identity);
+    }
+}
